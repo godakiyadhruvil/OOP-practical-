650d29d59bcadfbab18163e893ccbe625fa28f0d
godakiyadhruvil <godakiyadhruvil@gmail.com>
2026-04-22 10:08:52 +0530

    Create Practical3.java

---
+++ Set 1/Practical3.java
@@ -0,0 +1,15 @@
+public class CommandLineCalc {
+    public static void main(String[] args) {
+        if (args.length != 2) {
+            System.out.println("Please provide exactly two numbers.");
+            return;
+        }
+
+        double num1 = Double.parseDouble(args[0]);
+        double num2 = Double.parseDouble(args[1]);
+
+        double sum = num1 + num2;
+
+        System.out.println("Result: " + sum);
+    }
+}
